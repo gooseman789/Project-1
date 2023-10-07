a9cf04f25c4c3e9ec2814dcfbd6549f870079f1b
cliffordrouzeau <142095138+cliffordrouzeau@users.noreply.github.com>
2023-10-07 06:45:41 -0400

    fix

--- assets/script.js
+++ assets/script.js
@@ -9,32 +9,16 @@ BTN.on('click', function () {
         return response.json();
     })
     .then(function (data) {
-        if(data.data.length == 1){
+        console.log(data);
         var info = [data.data[0].title_english, data.data[0].episodes, data.data[0].rating, data.data[0].status, data.data[0].synopsis, data.data[0].year, data.data[0].images.jpg.large_image_url]
-        var genres =[]
-        for(i=0; i <data.data[0].genres.length; i++){
-            genres.push(data.data[0].genres[i].name)
-        }
-    
+        console.log(info)
         $('#title').text(info[0])
         $('#episodes').text(info[1] + " - Episodes")
         $('#rating').text(info[2])
         $('#status').text(info[3])
         $('#synopsis').text(info[4])
         $('#date').text(info[5])
-        $('#genre').text(genres)
         $( "#pic" ).attr('src', info[6]);
-        } else {
-            $('#title').text("NO DATA FOUND")
-            $('#episodes').text("NO DATA FOUND")
-            $('#rating').text("NO DATA FOUND")
-            $('#status').text("NO DATA FOUND")
-            $('#synopsis').text("NO DATA FOUND")
-            $('#date').text("NO DATA FOUND")
-            $('#genre').text("NO DATA FOUND")
-            $( "#pic" ).attr('src', "https://t4.ftcdn.net/jpg/04/75/01/23/360_F_475012363_aNqXx8CrsoTfJP5KCf1rERd6G50K0hXw.jpg");
-
-        }
         
     })
     fetch('https://api.watchmode.com/v1/search/?apiKey=kAkSWPYcQeHHMrCJU5ONXuwRIH85vNjxsRjumdtX&search_field=name&search_value=' + UserI.val(), {
@@ -81,7 +65,7 @@ BTN.on('click', function () {
             })
             .then(function(data3) {
                 var simSHOW1 = data3.title
-                console.log(simSHOW1)
+                // console.log(simSHOW1)
             })
             fetch('https://api.watchmode.com/v1/title/' + simTITLE2 + '/details/?apiKey=kAkSWPYcQeHHMrCJU5ONXuwRIH85vNjxsRjumdtX&append_to_response=sources', {
                 method: 'GET',
@@ -91,7 +75,7 @@ BTN.on('click', function () {
             })
             .then(function(data4) {
                 var simSHOW2 = data4.title
-                console.log(simSHOW2)
+                // console.log(simSHOW2)
             })
         })
 
